194142015f525d85f9f49a9a9fac653787b49213
era <me@elias.sh>
2023-04-11 17:40:36 +0100

    making math operations work

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use once_cell::sync::Lazy;
 use pest::iterators::{Pair, Pairs};
+use pest::pratt_parser::{Assoc, Op, PrattParser};
 use pest::Parser;
 
 #[derive(Parser)]
@@ -14,6 +16,15 @@ pub enum ParseError {
     Generic(String),
     InvalidNode(String),
 }
+
+// explanation on Pratt parsers in Rust:
+// https://matklad.github.io/2020/04/13/simple-but-powerful-pratt-parsing.html
+const PRATT_PARSER_MATH: Lazy<PrattParser<Rule>> = Lazy::new(|| {
+    PrattParser::new()
+        .op(Op::infix(Rule::add, Assoc::Left) | Op::infix(Rule::sub, Assoc::Left))
+        .op(Op::infix(Rule::mul, Assoc::Left) | Op::infix(Rule::div, Assoc::Left))
+        .op(Op::prefix(Rule::neg))
+});
 
 pub fn eval(input: &str) -> Result<String, ParseError> {
     let pairs = parse(input)?.next().unwrap().into_inner();
@@ -35,7 +46,7 @@ fn visit_print(node: Pairs<Rule>) -> Result<String, ParseError> {
     let mut result = String::new();
     for child in node {
         let child_result = match child.as_rule() {
-            Rule::exp =>visit_exp(child.into_inner())?,
+            Rule::exp => visit_exp(child.into_inner())?,
             _ => {
                 return Err(ParseError::InvalidNode(
                     format!("print statement does not accept {:?}", child).to_owned(),
@@ -52,39 +63,57 @@ fn visit_exp(node: Pairs<Rule>) -> Result<String, ParseError> {
     let first_child = node.into_iter().next().unwrap();
     match first_child.as_rule() {
         Rule::lang_types => visit_lang_types(first_child.into_inner()),
-        // Rule::op_add => visit_op_add(first_child.into_inner()),
-        //TODO Rule::op_add =>
-        _ => Err(ParseError::InvalidNode(format!("visit_exp was called with an invalid node {:?}", first_child)))
+        Rule::math => Ok(visit_math(first_child.into_inner())?.to_string()),
+        _ => Err(ParseError::InvalidNode(format!(
+            "visit_exp was called with an invalid node {:?}",
+            first_child
+        ))),
     }
 }
 
-fn visit_op_add<'a>(node: Pairs<Rule>) -> Result<String, ParseError> {
-    //TODO next live
-    // first lhs
-    // second operation (two choices)
-    // third rhs
-    // TODO
-    todo!()
+fn visit_math(pairs: Pairs<Rule>) -> Result<f32, ParseError> {
+    let result = PRATT_PARSER_MATH
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::integer | Rule::float => primary.as_str().parse().unwrap(),
+            Rule::math => visit_math(primary.into_inner()).unwrap(), // from "(" ~ math ~ ")"
+            _ => unreachable!(),
+        })
+        .map_prefix(|op, rhs| match op.as_rule() {
+            Rule::neg => -rhs,
+            _ => unreachable!(),
+        })
+        .map_infix(|lhs, op, rhs| match op.as_rule() {
+            Rule::add => lhs + rhs,
+            Rule::sub => lhs - rhs,
+            Rule::mul => lhs * rhs,
+            Rule::div => lhs / rhs,
+            _ => unreachable!(),
+        })
+        .parse(pairs);
+
+    Ok(result)
 }
 
 fn visit_lang_types<'a>(node: Pairs<Rule>) -> Result<String, ParseError> {
     for child in node {
         // we know for now that this resolves to a single child
         return match child.as_rule() {
-            //TODO actually return our own types, e.g. TinyLangTypes::Integer(i64)
-            Rule::integer => Ok(child.as_str().to_string()),
-            _ => Err(ParseError::InvalidNode(format!("visit_lang_types was called with an invalid node {:?}", child)))
+            Rule::integer | Rule::float => Ok(child.as_str().to_string()),
+            _ => Err(ParseError::InvalidNode(format!(
+                "visit_lang_types was called with an invalid node {:?}",
+                child
+            ))),
         };
     }
-    Err(ParseError::InvalidNode(format!("visit_lang_types was called with an empty node")))
+    Err(ParseError::InvalidNode(format!(
+        "visit_lang_types was called with an empty node"
+    )))
 }
 
 pub fn parse(input: &str) -> Result<Pairs<Rule>, ParseError> {
     // this is G which contains children which are
     // either html chars, print statements or dynamic statements
-    TemplateLangParser::parse(Rule::g, input).map_err(|e| {
-        ParseError::Generic(e.to_string())
-    })
+    TemplateLangParser::parse(Rule::g, input).map_err(|e| ParseError::Generic(e.to_string()))
 }
 
 fn main() {}
@@ -119,5 +148,40 @@ mod test {
     fn test_literal_print_stmt() {
         let result = eval("{{ 12 }}").unwrap();
         assert_eq!("12", result.as_str());
+    }
+
+    #[test]
+    fn test_literal_float_print_stmt() {
+        let result = eval("{{ 12.4 }}").unwrap();
+        assert_eq!("12.4", result.as_str());
+    }
+
+    #[test]
+    fn test_literal_float_math_print_stmt() {
+        let result = eval("{{ 12.4 + 4 }}").unwrap();
+        assert_eq!("16.4", result.as_str());
+    }
+
+    #[test]
+    fn test_math_add_print_stmt() {
+        let result = eval("{{ 12 + 1 }}").unwrap();
+        assert_eq!("13", result.as_str());
+    }
+    #[test]
+    fn test_math_neg_print_stmt() {
+        let result = eval("{{ -12 + 1 }}").unwrap();
+        assert_eq!("-11", result.as_str());
+    }
+
+    #[test]
+    fn test_math_mul_print_stmt() {
+        let result = eval("{{ 12 * 2 }}").unwrap();
+        assert_eq!("24", result.as_str())
+    }
+
+    #[test]
+    fn test_right_assoc_math_print_stmt() {
+        let result = eval("{{ 1 + 12 / 2 - 2 }}").unwrap();
+        assert_eq!("5", result.as_str())
     }
 }
